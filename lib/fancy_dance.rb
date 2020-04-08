18a04ade4bcf5e50442042fda8855166be372a79
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-08 23:47:50 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -3,9 +3,9 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 module FancyDance
-  include Dance 
+  include Dance
   extend  metadata
-  
+
   module InstanceMethods              # you "include" these instance methods
     def backflip
       "the instance method just did a backflip"
