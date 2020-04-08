8a15cb145187dfa5cecc2f88368e16414864da5b
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-08 23:45:18 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -1,7 +1,15 @@
 
 require_relative './dance_module.rb'
 
-class FancyDance
-  include Dance
-  extend MetaDancing
+module FancyDance
+  module InstanceMethods              # you "include" these instance methods
+    def backflip
+      "I'm just one jumping instance"
+    end
+  end
+  module ClassMethods                 # you "extend" these class methods
+    def frontflip
+      "I'm a whole class of MOFO's jumping"
+    end
+  end 
 end
