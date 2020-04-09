7edead48af88eb38caed4966c5b1ae6462ca0ee1
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-09 00:02:50 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -2,14 +2,29 @@
 #require_relative './dance_module.rb'
 
 module FancyDance
-  module InstanceMethods              # you "include" these instance methods
-    def backflip
-      "the instance method just did a backflip"
+  module InstanceMethods
+ 
+    def twirl
+      "I'm twirling!"
+    end
+ 
+    def jump
+      "Look how high I'm jumping!"
+    end
+ 
+    def pirouette
+      "I'm doing a pirouette"
+    end
+ 
+    def take_a_bow
+      "Thank you, thank you. It was a pleasure to dance for you all."
     end
   end
-  module ClassMethods                 # you "extend" these class methods
-    def frontflip
-      "the class method just did a frontflip"
+ 
+  module ClassMethods
+ 
+    def metadata
+      "This class produces objects that love to dance."
     end
   end
 end
